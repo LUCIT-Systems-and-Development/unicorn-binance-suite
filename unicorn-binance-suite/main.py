deadd271248d13faefd2b893debb2541d4981b86
Oliver Zehentleitner <47597331+oliver-zehentleitner@users.noreply.github.com>
2022-03-04 14:42:22 +0100

    Update main.py

---
+++ unicorn-binance-suite/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# File: unicorn-binance-suite/main.py
+#
+# Part of ‘UNICORN Binance Suite’
+# Project website: https://www.lucit.tech/unicorn-binance-suite.html
+# Github: https://github.com/LUCIT-Systems-and-Development/unicorn-binance-suite
+# Documentation: https://unicorn-binance-suite.docs.lucit.tech
+# PyPI: https://pypi.org/project/unicorn-binance-suite
+#
+# Author: LUCIT Systems and Development
+#
+# Copyright (c) 2022-2022, LUCIT Systems and Development (https://www.lucit.tech) and Oliver Zehentleitner
+# # All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
